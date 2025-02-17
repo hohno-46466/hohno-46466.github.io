7af354de62209a7b3be336f737941d910df1d15a
hohno-46466 <hohno.46466@gmail.com>
2025-02-18 04:55:11 +0900

    updated by @hohno_at_kuimc

--- Processing4/sketch_20241115a_POTdemo4/pseudoBob1.sh
+++ Processing4/sketch_20241115a_POTdemo4/pseudoBob1.sh
@@ -5,6 +5,6 @@ BROKER="broker.emqx.io"
 
 x=0;
 while [ 1 ]; do x=$(($x + 1)); echo $x; sleep 0.1; done \
-| awk '{x=$1;y=($1*10)%256;z=255-x;w=255-y; printf "%d %d %d %d\n", x,y,z,w; fflush()}' \
+| awk '{x=$1%256;y=($1*10)%256;z=255-x;w=255-y; printf "%d %d %d %d\n", x,y,z,w; fflush()}' \
 | mosquitto_pub -l -t "$TOPIC" -h "$BROKER"
 
