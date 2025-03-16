669a66f2fcc542d967474c599cb441bca6c6d751
hohno-46466 <hohno.46466@gmail.com>
2025-03-16 17:10:21 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2022P.local

--- c/00/tools/recv-mesg.sh
+++ c/00/tools/recv-mesg.sh
@@ -13,32 +13,27 @@ XCMD2="send-offset.sh"
 XCMD0A="ntpdate -q ntp.nict.jp | grep offset | tail -1 | awk '{printf \"%.3f\", \$(NF-1)}'"
 XCMD0B="/usr/sbin/ntpdig ntp.nict.jp | awk '{printf \"%.2f\", \$4}'"
 
+# ntpdig コマンドが存在するか確認
+if command -v ntpdig &> /dev/null; then
+    CMD0="ntpdig ntp.nict.jp | awk '{printf \"%.2f\", \$4}'"
 # ntpdate コマンドが存在するか確認
-if command -v ntpdate &> /dev/null; then
- CMD0="$XCMD0A"
-# ntpdate がない場合、/usr/sbin/ntpdig コマンドが存在するか確認
-elif command -v /usr/sbin/ntpdig &> /dev/null; then
- CMD0="$XCMD0B"
+elif command -v ntpdate &> /dev/null; then
+    CMD0="ntpdate -q ntp.nict.jp | grep offset | tail -1 | awk '{printf \"%.3f\", \$(NF-1)}'"
+# /usr/sbin/ntpdig コマンドが存在するか確認
+elif command -v /usr/sbin/ntpdate &> /dev/null; then
+    CMD0="/usr/sbin/ntpdate -q ntp.nict.jp | grep offset | tail -1 | awk '{printf \"%.3f\", \$(NF-1)}'"
 else
-  echo "Error: Neither ntpdate nor ntpdig is installed."
-  exit 1
+    echo "Error: Neither ntpdate nor ntpdig is installed."
+    exit 1
 fi
 
-# awk にコマンドを渡して実行
-awk -v command="$CMD0" 'BEGIN {
-    command | getline timediff
-    close(command)
-    printf "%s\n",timediff
-}'
-
-exit 
-
-z=""
-[ -z "$z" ] && x=$(which "ntpdate") &&  [ ! -z $x ] && z="$x"
-[ ! -z "$z" ] && CMD0="$XCMD0A"
-[ -z "$z" ] && x=$(which "/usr/sbin/ntpdig") &&  [ ! -z $x ] && z="$x"
-[ ! -z "$z" ] && CMD0="$XCMD0B"
-[ -z "$z" ] && echo "NG: Can't find ntpdate nor ntpdig" && exit 1
+# # awk にコマンドを渡して実行
+# awk -v command="$CMD0" 'BEGIN {
+#     command | getline timediff
+#     close(command)
+#     printf "%s\n",timediff
+# }'
+# exit 
 
 z=""
 [ -z "$z" ] && x=$(find . -name "$XCMD1" -type f -perm -100) && [ ! -z $x ] && z="$x"
@@ -59,11 +54,9 @@ CMD2="$z"
 echo "(Debug) [$CMD0]"
 echo "(Debug) [$CMD1][$CMD2]"
 
-exit
-
 mosquitto_sub -t "$TOPIC/$XK" -h "$HOST" \
 | while read x; do echo "$x $(date +%s.%3N)"; done \
-| awk -v command="$CMD0" -v CMD1="$CMD1" -v CMD2="$CMD2" '
+| awk -v CMD0="$CMD0" -v CMD1="$CMD1" -v CMD2="$CMD2" '
 BEGIN{
   myhash = "";
   adjval = 0;
@@ -89,7 +82,7 @@ BEGIN{
     T1 = ($3 + $5)/2;
     T2 = $4 - T1;
     if ($2 == myhash) {
-      command = "$CMD0"
+      command = CMD0
       command | getline ntpdiff;
       # 重要：ntpdiff の値が正ならローカルPCは NTPサーバより遅れて（NTPサーバの方が進んで）おり、負ならその逆である
       close(command);
@@ -100,7 +93,7 @@ BEGIN{
       # T3 = $4 - ($3 + $5)/2 - 0.3;
       T3 = T2 + adjval;
       T4 = -1 * T3;
-      mesg = "$CMD2 " $2 " " T4;
+      mesg = CMD2 " " $2 " " T4;
       printf "(Debug/pongB) (%s) => (T1:%.2f) (T2:%.2f) (adj:%.2f) (T3:%.2f) (T4:%.2f)\n", $0, T1, T2, adjval, T3, T4;
       if (T4 <= -0.1 || T4 >= 0.1) {
         printf "(Debug/pongB) [%s]\n", mesg;
@@ -108,7 +101,7 @@ BEGIN{
       }
   } else if($1 == "Hello!") {
     printf "(Debug/Hello!) (%s)\n",$0
-    mesg = "$CMD1 " $2;
+    mesg = CMD1 " " $2;
     printf "(Debug/Hello!) [%s]\n", mesg;
     system(mesg);
   } else {
