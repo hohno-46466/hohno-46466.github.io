252b4e532e0726851d48f2695f6778d0735f1b8c
hohno-46466 <hohno.46466@gmail.com>
2025-03-16 16:55:28 +0900

    updated by @hohno_at_kuimc on penguin

--- c/00/tools/afo.sh
+++ c/00/tools/afo.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# ntpdate コマンドが存在するか確認
-if command -v ntpdate &> /dev/null; then
-    CMD="ntpdate -q ntp.nict.jp | grep offset | tail -1 | awk '{printf \"%.3f\", \$(NF-1)}'"
-# ntpdate がない場合、ntpdig コマンドが存在するか確認
-elif command -v /usr/sbin/ntpdig &> /dev/null; then
-    CMD="/usr/sbin/ntpdig ntp.nict.jp | awk '{printf \"%.2f\", \$4}'"
+if command -v ntpdig &> /dev/null; then
+    CMD0="ntpdig ntp.nict.jp | awk '{printf \"%.2f\", \$4}'"
+elif command -v ntpdate &> /dev/null; then
+    CMD0="ntpdate -q ntp.nict.jp | grep offset | tail -1 | awk '{printf \"%.3f\", \$(NF-1)}'"
+elif command -v /usr/sbin/ntpdate &> /dev/null; then
+    CMD0="/usr/sbin/ntpdate -q ntp.nict.jp | grep offset | tail -1 | awk '{printf \"%.3f\", \$(NF-1)}'"
 else
     echo "Error: Neither ntpdate nor ntpdig is installed."
     exit 1
 fi
 
-echo "[$CMD]"
+echo "[$CMD0]"
 
 # awk にコマンドを渡して実行
-awk -v command="$CMD" 'BEGIN {
+awk -v command="$CMD0" 'BEGIN {
     command | getline timediff
     close(command)
     printf "%s\n",timediff
