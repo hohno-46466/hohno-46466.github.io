e3275aadb9aebe0d56e68b2179ac7d23054a8c3e
hohno-46466 <hohno.46466@gmail.com>
2025-04-09 04:31:29 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2022P.local

--- p5.js/test01/sketch-010.js
+++ p5.js/test01/sketch-010.js
@@ -1,27 +1,4 @@
-// No.013
-const version = "No.013";
-const client = mqtt.connect('ws://localhost:9001');
-
-client.on('connect', () => {
-  console.log('MQTT connected');
-});
-
-client.on('error', (err) => {
-  console.error('MQTT error:', err);
-});
-
-client.on('reconnect', () => {
-  console.warn('MQTT reconnecting...');
-});
-
-client.on('close', () => {
-  console.warn('MQTT connection closed');
-});
-
-client.on('offline', () => {
-  console.warn('MQTT client is offline');
-});
-
+// No.010
 let x = 0;
 let pos = 50;
 let min = 0;
@@ -30,15 +7,6 @@ let labelText = '';
 let shapeType = 'circle';
 let r = 255, g = 0, b = 0;
 let speed = 2;
-
-function publishState() {
-  if (client.connected) {
-    const topic = "WStest/" + version;
-    const payload = version + "\t" + speed + "\t" + pos + "\t" + r + "\t" + g + "\t" + b;
-    console.log(payload);
-    client.publish(topic, payload);
-  }
-}
 
 function setup() {
   const container = document.getElementById("canvas-container");
@@ -60,23 +28,21 @@ function setup() {
 
   const posSlider = document.getElementById("pos-slider");
   posSlider.addEventListener("input", () => {
-  pos = Number(posSlider.value);
-  publishState();
+    pos = Number(posSlider.value);
   });
 
   const speedSlider = document.getElementById("speed-slider");
   speedSlider.addEventListener("input", () => {
     speed = Number(speedSlider.value);
-    publishState();
   });
 
   const rSlider = document.getElementById("r-slider");
   const gSlider = document.getElementById("g-slider");
   const bSlider = document.getElementById("b-slider");
 
-  rSlider.addEventListener("input", () => { r = Number(rSlider.value); publishState();});
-  gSlider.addEventListener("input", () => { g = Number(gSlider.value); publishState();});
-  bSlider.addEventListener("input", () => { b = Number(bSlider.value); publishState();});
+  rSlider.addEventListener("input", () => { r = Number(rSlider.value); });
+  gSlider.addEventListener("input", () => { g = Number(gSlider.value); });
+  bSlider.addEventListener("input", () => { b = Number(bSlider.value); });
 }
 
 function draw() {
@@ -84,6 +50,7 @@ function draw() {
   fill(r, g, b);
   noStroke();
 
+  const margin = width * 0.1;
   const xpos = x;
   const ypos = map(pos, min, max, height * 0.1, height * 0.9);
 
