a1b3b60c365cd532b4187882826e2854e7c466e3
hohno-46466 <hohno.46466@gmail.com>
2025-08-10 11:57:06 +0900

    added by @hohno_at_kuimc on hohno-MBP-2022P.local

---
+++ Processing4/sketch_20241115a_POTdemo4_GPT/pseudoBob1.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+TOPIC="mgws2411-999/pseudoBob1"
+BROKER="broker.emqx.io"
+
+x=0;
+while [ 1 ]; do x=$(($x + 1)); echo $x; sleep 0.1; done \
+| awk '{x=$1%256;y=($1*10)%256;z=255-x;w=255-y; printf "%d %d %d %d\n", x,y,z,w; fflush()}' \
+| mosquitto_pub -l -t "$TOPIC" -h "$BROKER"
+
